4df21fe0db95d3286d6737e61c12cad1e20c449d
Matthew <matthew.stewart827@gmail.com>
2023-09-18 11:54:06 -0500

    complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
-# your code goes here!
+class Anagram:
+    
+    def __init__(self, word):
+        self.word_letters = sorted([letter for letter in word])
+        
+    def match(self, word_list):
+        match_words = []
+        
+        for word in word_list:
+            if sorted([letter for letter in word]) == self.word_letters:
+                match_words.append(word)
+
+        return match_words
